f84acd94bbefee54489abb7aa252891acf2d7184
root <root@kakisaba>
2023-10-04 23:21:36 +0900

    20231004 edit

---
+++ click.js
@@ -0,0 +1,12 @@
+document.getElementById("form-button").onclick = function() {
+  document.getElementById("textdata").innerHTML = document.getElementById("inputdate").value
+  };
+
+document.getElementById("form-button").onclick = function() {
+  document.getElementById("texttime").innerHTML = document.getElementById("inputtime").value
+  };
+
+document.getElementById("form-button").onclick = function() {
+  document.getElementById("textnum").innerHTML = document.getElementById("inputnumber").value
+  };
+  
